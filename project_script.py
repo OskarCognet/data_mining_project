4cfd52f5c528162660b5913d5ef794c93d277ea9
Oskar Cognet <oskar.cognet@tpi21.cpe.lan>
2022-03-16 16:06:48 +0100

    Ajout d'images au dossier images

--- project_script.py
+++ project_script.py
@@ -9,13 +9,13 @@ except FileExistsError:
 
 #{"username":"oskarcognet","key":"929baddf391d0a06468ca27bee3c435b"}
 
-!pip install kaggle
+#!pip install kaggle
 api_token = {"username":"oskarcognet","key":"929baddf391d0a06468ca27bee3c435b"}
 with open('/content/.kaggle/kaggle.json', 'w') as file:
     json.dump(api_token, file)
-!chmod 600 /content/.kaggle/kaggle.json
-!kaggle config path -p /content
-!kaggle competitions download -c jigsaw-toxic-comment-classification-challenge
+#!chmod 600 /content/.kaggle/kaggle.json
+#!kaggle config path -p /content
+#!kaggle competitions download -c jigsaw-toxic-comment-classification-challenge
 os.chdir('/content/competitions/jigsaw-toxic-comment-classification-challenge')
 for file in os.listdir():
     zip_ref = zipfile.ZipFile(file, 'r')
